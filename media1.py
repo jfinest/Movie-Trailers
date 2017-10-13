ecce958f4c31162daf366125e0fcc46730f84dd0
JosueAcosta <josueacosta@IsidroskBookPro.home>
2017-10-13 19:09:21 -0400

    style: deleted any trailing whitespaces

--- media1.py
+++ media1.py
@@ -1,17 +1,13 @@
 import webbrowser
 
-class Movie():         
-	"""         
-		This class will be used to create a list
-		of movies which will show title, a movie poster and a trailer in the 
-		browser to the user.      
-	"""          
-	def __init__(self, movie_title, poster_image,
-				trailer_youtube):
-		"""
-			This is a constructor that takes 3 parameter. The movie title, 
-			movie poster image url and movie youtube trailer url.
-		"""              
+
+class Movie():
+	""" This class will be used to create a list of movies which will
+	show title, a movie poster and a trailer in the browser to the user. """
+
+	def __init__(self, movie_title, poster_image, trailer_youtube):
+		""" This is a constructor that takes 3 parameter. The movie title,
+		movie poster image url and movie youtube trailer url. """
 		self.title = movie_title
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = trailer_youtube
